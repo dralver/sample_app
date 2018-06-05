73f02282a1718a8610c6bad2763cf336317bc3cc
Marina Marinova <marinaaltfortrash@gmail.com>
2018-06-06 01:24:57 +0300

    Finished Chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
-  def full_title(content)
-    content + ' | Ruby on Rails Tutorial Sample App'
+  def full_title(content = '')
+    result = 'Ruby on Rails Tutorial Sample App'
+    if content.length > 0
+      result = content + ' | ' + result
+    end
+
+    result
   end
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full_title_helper" do
-    assert_equal full_title, 'Ruby on Rails Tutorial'
-    assert_equal full_title('Help'), 'Ruby on Rails Tutorial | Help'
+    assert_equal full_title, 'Ruby on Rails Tutorial Sample App'
+    assert_equal full_title('Help'), 'Help | Ruby on Rails Tutorial Sample App'
   end
 end
